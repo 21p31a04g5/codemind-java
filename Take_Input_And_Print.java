9554b2f74f6d556511a5ea3f500d1141d11aa274
21p31a04g5 <116954796+21p31a04g5@users.noreply.github.com>
2023-05-20 23:36:34 +0530

    import java.util.Scanner; public class ipop {     public static void main(String args[])     {         String a;         Scanner sc=new Scanner(System.in);         a=sc.nextLine();
            System.out.println(a);
            sc.close();
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ipop
+{
+    public static void main(String args[])
+    {
+        String a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        
+        System.out.println(a);
+        sc.close();
+    }
+}
