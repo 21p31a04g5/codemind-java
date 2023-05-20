772361629d976e416bbab9af5afd3d7b1b8625e8
21p31a04g5 <116954796+21p31a04g5@users.noreply.github.com>
2023-05-20 23:26:54 +0530

    import java.util.*; public class lo {     public static void main(String[] args)     {         Scanner g=new Scanner(System.in);         char c=g.next().charAt(0);         int a=c;         System.out.println(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class lo
+{
+    public static void main(String[] args)
+    {
+        Scanner g=new Scanner(System.in);
+        char c=g.next().charAt(0);
+        int a=c;
+        System.out.println(a);
+    }
+}
